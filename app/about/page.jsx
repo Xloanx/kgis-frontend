2a1b39f76aed71a912480585cf26d34e3536c738
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-03-15 18:31:56 +0100

    implemented about

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+export default function About() {
+  return (
+     <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+            <h1>About Us</h1>
+        </main>
+    </div>
+  );
+}
---
+++ components/about.jsx
@@ -0,0 +1,43 @@
+'use client'
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const About = () => {
+  return (
+    <motion.section 
+      className="py-20 bg-gray-100 text-gray-900"
+      initial={{ opacity: 0, x: -100 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+    >
+      <div className="container mx-auto text-center">
+        <motion.h2 
+          className="text-4xl font-bold mb-6"
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          About Us
+        </motion.h2>
+        <motion.p 
+          className="max-w-2xl mx-auto"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Founded by passionate technologists, Kernel Greens is committed to 
+          delivering cutting-edge solutions that empower businesses. 
+          We specialize in software development, IT support, and cloud operations, 
+          ensuring that your digital initiatives thrive. <br />
+          With a team of skilled professionals and a commitment to excellence, 
+          we are dedicated to helping you navigate the ever-evolving landscape of 
+          technology...
+          <Link href="/about" className="text-gray-600 hover:text-accent font-semibold hover:underline">Learn More</Link>
+        </motion.p>
+      </div>
+    </motion.section>
+  );
+};
+
+export default About;
