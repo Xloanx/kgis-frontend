843df2f6db7e7b767f73d8c078427de0f023b5d7
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-03-15 18:31:08 +0100

    landing page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +28,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
